c1bbebf48ef958d927ef65eb0739c9d4dafbb9df
Izaltino <izaltinoromaoneto@hotmail.com>
2024-11-18 09:21:47 -0500

    Initial Commit

---
+++ HowToMath/HowToMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToMathApp.swift
+//  HowToMath
+//
+//  Created by user269369 on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct HowToMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
